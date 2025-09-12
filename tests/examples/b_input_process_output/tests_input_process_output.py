4dd36fe9fcb5337e69bd492afd64b027bad8a274
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-09-12 01:11:22 +0000

    string concatenation

--- tests/examples/b_input_process_output/tests_input_process_output.py
+++ tests/examples/b_input_process_output/tests_input_process_output.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.examples.b_input_proc_output.input_process_output import integer_division, test_config, float_division
+from src.examples.b_input_proc_output.input_process_output import integer_division, test_config, float_division, string_concatenation, string_concatenation_params
 
 class Test_Config(unittest.TestCase):
 
@@ -16,4 +16,22 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(2, integer_division(5,2))
         self.assertEqual(3, integer_division(7,2))
         self.assertEqual(5, integer_division(10,2))
+
+    def test_string_concatenation_no_params(self):
+        self.assertEqual(string_concatenation(), "Hello World")
+
+    def test_string_concatenation_params_basic(self):
+        self.assertEqual(string_concatenation_params("Hello", "World"), "Hello World")
+
+    def test_string_concatenation_params_empty(self):
+        self.assertEqual(string_concatenation_params("", ""), " ")
+
+    def test_string_concatenation_params_numbers(self):
+        self.assertEqual(string_concatenation_params("123", "456"), "123 456")
+
+    def test_string_concatenation_params_special_chars(self):
+        self.assertEqual(string_concatenation_params("foo!", "@bar"), "foo! @bar")    
+
+
+    
 
