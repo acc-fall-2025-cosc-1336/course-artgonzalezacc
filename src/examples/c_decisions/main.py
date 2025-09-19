e945b04276b457928f9a0f31f3254225fbf80332
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-09-19 00:46:21 +0000

    intro to if statement

--- src/examples/c_decisions/main.py
+++ src/examples/c_decisions/main.py
@@ -1,11 +1,13 @@
 from decisions import is_vowel
 
 def main():
-    print(is_vowel('a'))
-    print(is_vowel('b'))
-    print(is_vowel('w'))
-    print(is_vowel('x'))
-    print(is_vowel('y'))
+    char = input('Enter a letter: ')
+
+    result = is_vowel(char)
+
+    if(result):
+        print(char, 'is vowel')
+   
 
 if __name__ == "__main__":
     main()
