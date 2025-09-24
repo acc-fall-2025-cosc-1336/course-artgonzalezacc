1074a7ccdc5eddcdf35f204e6a14b5d756c5ba80
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-09-24 00:56:14 +0000

    while display hello n times

--- src/examples/d_repetition/repetition.py
+++ src/examples/d_repetition/repetition.py
@@ -1,2 +1,15 @@
 def test_config():
     return True
+
+def display_hello(num):
+    counter = 0
+    
+    print('before loop')
+
+    while counter < num:
+        print(counter, num, counter < num, "Hello")
+        counter = counter + 1
+        if(counter > 2):
+            print(counter, num, counter < num)
+
+    print('after loop')
