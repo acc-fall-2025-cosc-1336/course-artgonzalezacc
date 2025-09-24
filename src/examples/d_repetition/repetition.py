f466515799dc6de1d67b126acfe44b37be711ca8
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-09-24 01:20:52 +0000

    while sum values 1 to n

--- src/examples/d_repetition/repetition.py
+++ src/examples/d_repetition/repetition.py
@@ -13,3 +13,18 @@ def display_hello(num):
             print(counter, num, counter < num)
 
     print('after loop')
+
+# function that sums values from 1 to n
+def sum_to_n(n):
+    total = 0
+    counter = 0
+    
+    while counter <= n:
+        total += counter #total = total + counter
+        print(counter, n, counter <= n, total)
+        counter += 1
+
+        if(counter > n):
+            print(counter, n, counter <= n, total)
+
+    return total
--- tests/examples/d_repetition/tests_repetition.py
+++ tests/examples/d_repetition/tests_repetition.py
@@ -1,9 +1,16 @@
 import unittest
 
-from src.examples.d_repetition.repetition import test_config
+from src.examples.d_repetition.repetition import test_config, sum_to_n
 
 class Test_Config(unittest.TestCase):
 
     def test_configuration(self):
         self.assertEqual(True, test_config())
+
+    def test_sum_to_n(self):
+        self.assertEqual(6, sum_to_n(3))
+        #self.assertEqual(10, sum_to_n(4))
+        #self.assertEqual(15, sum_to_n(5))
+        #self.assertEqual(55, sum_to_n(10))
+        #self.assertEqual(5050, sum_to_n(100))
 
