11ae8d15598dfe382b0c5bcdb986bac41dceda01
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-10-03 01:27:50 +0000

    overwrite a global variable value

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -1,7 +1,8 @@
 #main program
-from void_functions import display_num, print_global_variable, say_hello
+from void_functions import display_num, print_global_variable, say_hello, change_global_variable_value
 
 def main():
+    change_global_variable_value()
     print_global_variable()
     display_num(5)
     say_hello()
--- src/examples/e_functions/void_functions.py
+++ src/examples/e_functions/void_functions.py
@@ -2,16 +2,18 @@
 state_sales_tax_rate = .085
 
 def say_hello():
-    print('hello')
     print(state_sales_tax_rate)
 
 def display_num(num):
-    print(num)
-    num = 20
-    print(id(num))
     print(state_sales_tax_rate)
 
 def print_global_variable():
     state_sales_tax_rate = 1 #created a function/local variable IT'S NOT USING THE GLOBAL VARIABLE
+    print(state_sales_tax_rate)
+
+def change_global_variable_value():
+    global state_sales_tax_rate #use global variable
+    state_sales_tax_rate = .09 #overwrite the global variable
+    
     print(state_sales_tax_rate)
 
