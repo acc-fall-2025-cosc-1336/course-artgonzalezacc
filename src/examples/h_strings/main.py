e4e8cd407b69d7b0e52930cdf9467481c13fb89a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-10-10 01:22:29 +0000

    string negative  indexes

--- src/examples/h_strings/main.py
+++ src/examples/h_strings/main.py
@@ -1,16 +1,9 @@
 #main program
-from strings import get_x_char_cnt_of_string
 
 def main():
-    name = "C++ C# Java and Python"
-    choice = 'Y'
-
-    while(choice == 'Y' or choice == 'y'):
-        char = input("Enter the character to count: ")
-        count = get_x_char_cnt_of_string(name, char)
-        print(f"The character '{char}' appears {count} times in the string.")
-        choice = input("Do you want to continue? (Y/N): ")
-        
-
+    name = "Can"
+    #print(name[3])#generates an error
+    print(name[-2])#print the second to last character
+   
 if __name__ == "__main__":
     main()
