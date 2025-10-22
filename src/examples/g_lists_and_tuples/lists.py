dec1899ea987958bb6d96b30831c17820a8c8131
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-10-22 01:13:20 +0000

    create list , indexing

--- src/examples/g_lists_and_tuples/lists.py
+++ src/examples/g_lists_and_tuples/lists.py
@@ -1,2 +1,8 @@
 def test_config():
     return True
+
+def list_indexing():
+    #index  0  1  2  3   4
+    nums = [2, 4, 6, 8, 10]
+    print(nums[2])  # display 6
+    print(nums[4])  # display 10
--- src/examples/g_lists_and_tuples/main.py
+++ src/examples/g_lists_and_tuples/main.py
@@ -1,1 +1,9 @@
 #main program
+from lists import list_indexing
+
+def main():
+
+    list_indexing()
+
+if __name__ == "__main__":
+    main()
