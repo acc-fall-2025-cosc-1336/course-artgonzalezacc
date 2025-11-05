fff850f18952ccebcd89aecfd8dfa2381bab689a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-11-05 01:42:09 +0000

    create tuple

--- src/examples/g_lists_and_tuples/main.py
+++ src/examples/g_lists_and_tuples/main.py
@@ -1,9 +1,8 @@
 #main program
-from lists import get_multiplication_table, display_multiplication_table
+from tuples import create_tuple
 
 def main():
-    table = get_multiplication_table(10,10)
-    display_multiplication_table(table)
+    create_tuple()
 
 if __name__ == "__main__":
     main()
--- src/examples/g_lists_and_tuples/tuples.py
+++ src/examples/g_lists_and_tuples/tuples.py
@@ -1,1 +1,4 @@
 #
+def create_tuple():
+    my_tuple = (8, 5, 9, 1, 4)
+    print(my_tuple)
