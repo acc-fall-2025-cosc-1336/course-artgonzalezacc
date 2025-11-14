ea5a859ce1063f83c110618889d74abc08dcb5f9
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-11-14 01:49:54 +0000

    access dictionary value

--- tests/examples/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/examples/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -7,3 +7,11 @@ class Test_Config(unittest.TestCase):
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
+    def test_access_dictionary_value(self):
+        phonebook = {'555-1111':'Chris', '555-2222':'Katie', '555-3333':'Joanne'}
+        expected_value = 'Katie'
+
+        value = phonebook['555-2222']
+
+        self.assertEqual(value, expected_value)
+
