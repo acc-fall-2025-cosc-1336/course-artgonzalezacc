18d65a38456061ade4daf45acb4781d03b8bcc94
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-11-21 02:02:14 +0000

    read write text files

--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -1,1 +1,9 @@
 #main program
+from files import write_to_file, read_from_file
+
+def main():
+    file_name = 'philosophers.txt'
+    #write_to_file(file_name)
+    read_from_file(file_name)
+
+main()
