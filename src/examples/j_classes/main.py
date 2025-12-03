35e2f114849250f12eea81e0b97de5d5fab55343
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-12-03 02:03:40 +0000

    use deposit n withdraw in main

--- src/examples/j_classes/main.py
+++ src/examples/j_classes/main.py
@@ -4,4 +4,13 @@ def main():
     account = BankAccount(1000) #create a variable of type balance
     print("balance: ", account.get_balance() ) #get the balance with get_balance
 
+    amt = int(input("Enter amount to deposit: "))
+
+    account.deposit(amt)
+    print("balance: ", account.get_balance() ) #get the balance with get_balance
+
+    amt = int(input("Enter amount to withdraw: "))
+    account.withdraw(amt)
+    print("balance: ", account.get_balance() ) #get the balance with get_balance
+
 main()
