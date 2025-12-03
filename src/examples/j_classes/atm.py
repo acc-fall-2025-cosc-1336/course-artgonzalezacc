8955d5f3bd6a8277b013bc3bee797fc0ac150bdc
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-12-03 02:31:04 +0000

    add atm menu

--- src/examples/j_classes/atm.py
+++ src/examples/j_classes/atm.py
@@ -16,4 +16,38 @@ class ATM:
         amt = int(input("Enter amount to withdraw: "))
         self.__account.withdraw(amt)
     
+
+#FREE FUNCTIONS-are not part of the ATM class
+
+def display_menu():
+    print('ACC Bank')
+    print('1-Display Balance')
+    print('2-Make Deposit')
+    print('3-Make Withdraw')
+    print('4-Exit')
+
+def run_menu(atm):
+
+    choice = '1'
+
+    while(choice != '4'):
+
+        display_menu()
+        choice = input('Enter menu choice: ')
+        handle_menu_option(atm, choice)
+
+
+def handle_menu_option(atm, choice):
+
+    if choice == '1':
+        atm.display_balance()
+    elif choice == '2':
+        atm.make_deposit()
+    elif choice == '3':
+        atm.make_withdraw()
+    elif choice == '4':
+        print('Exiting..')
+    else:
+        print('Invalid option.')
+
     
--- src/examples/j_classes/main.py
+++ src/examples/j_classes/main.py
@@ -1,27 +1,12 @@
 from bank_account import BankAccount
-from atm import ATM
+from atm import ATM, run_menu
 
 def main():
     account = BankAccount(1000) #create a variable of type balance
-    print("balance: ", account.get_balance() ) #get the balance with get_balance
-
-    atm = ATM(account)
-
-    atm.display_balance()
-
-    atm.make_deposit()
-
-    atm.display_balance()
-
-    atm.make_withdraw()
-
-    atm.display_balance()
-
-    account = BankAccount(500)
-
-    atm = ATM(account)
-
-    atm.display_balance()
     
+    atm = ATM(account)
+
+    run_menu(atm)
+
 
 main()
